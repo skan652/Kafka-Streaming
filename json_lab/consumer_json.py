c9a7d2a46ee26081bbc307d90dc902765355ae59
skan652 <skander.afi@dauphine.tn>
2025-12-16 16:37:29 +0100

    JSON

--- json_lab/consumer_json.py
+++ json_lab/consumer_json.py
@@ -1,1 +1,16 @@
 # Write your json consumer code here
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer(
+    "transactions_json",
+    bootstrap_servers="localhost:9092",
+    group_id="json-consumer-group",
+    auto_offset_reset="earliest",
+    value_deserializer=lambda v: v.decode("utf-8")
+)
+
+print("📥 JSON Consumer started")
+
+for msg in consumer:
+    print(f"Received ← {msg.value}")
+print("✅ JSON Consumer finished")
--- json_lab/producer_json.py
+++ json_lab/producer_json.py
@@ -1,1 +1,33 @@
-#write your json producer code here
+import csv
+import json
+import time
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+TOPIC = "transactions_json"
+
+print("📤 JSON Producer started")
+
+with open("../csv_lab/data/transactions.csv", "r", encoding="utf-8") as f:
+    reader = csv.reader(f)
+    header = next(reader)
+
+    for row in reader:
+        event = {
+            "transaction_id": int(row[0]),
+            "user_id": int(row[1]),
+            "amount": float(row[2]),
+            "timestamp": row[3]
+        }
+
+        producer.send(TOPIC, event)
+        print(f"Sent → {event}")
+        time.sleep(1)
+
+producer.flush()
+producer.close()
+print("✅ JSON Producer finished")
